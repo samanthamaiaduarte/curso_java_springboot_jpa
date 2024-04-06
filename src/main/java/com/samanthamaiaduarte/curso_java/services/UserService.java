bd922f3d2ace605296b32b6dab02d226293d193b
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-06 17:26:13 -0300

    User insert

--- src/main/java/com/samanthamaiaduarte/curso_java/services/UserService.java
+++ src/main/java/com/samanthamaiaduarte/curso_java/services/UserService.java
@@ -21,4 +21,8 @@ public class UserService {
 	public User findById(Long id) {
 		return repository.findById(id).get();
 	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
 }
