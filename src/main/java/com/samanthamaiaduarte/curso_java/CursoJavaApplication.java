671a2e220485d089994e2685819d87a6da922045
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-05 21:30:55 -0300

    Project created

---
+++ src/main/java/com/samanthamaiaduarte/curso_java/CursoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.samanthamaiaduarte.curso_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samanthamaiaduarte/curso_java/CursoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samanthamaiaduarte.curso_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
